e58582b8f13c247f04c7fa5c6c5324b56b62f7d7
Aida Tabiou <aidatabiou@yahoo.fr>
2025-07-23 18:04:52 +0200

    my first commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+st.title("This is my streamlit app")
+
+if st.button("Click me please"):
+    st.balloons()
+
+
+lower_year, higher_year = st.slider('Pick a year', min_value=1960, max_value=2010, value=(1965, 1970))
+
+
+nb_displayed = st.selectbox("Number of country to display", [3, 5, 10, 20, 30])
+
+df = pd.read_csv('data/CO2_per_capita.csv', sep=';')
+
+
+def top_n_emitters(df, start_year=2008, end_year=2011, nb_displayed=10):
+    # Filtrer les années
+    df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
+    # Moyenne des émissions par pays
+    df_mean = df.groupby('Country Name')['CO2 Per Capita (metric tons)'].mean().reset_index()
+    # Trier les valeurs
+    df_mean = df_mean.sort_values(by='CO2 Per Capita (metric tons)', ascending=False).head(nb_displayed)
+    # Créer la figure avec Plotly
+    fig = go.Figure(data=[
+        go.Bar(
+            x=df_mean['Country Name'],
+            y=df_mean['CO2 Per Capita (metric tons)']
+        )
+    ])
+    # Mettre en forme
+    fig.update_layout(
+        title=f'Top {nb_displayed} CO2 emitters from {start_year} to {end_year}',
+        xaxis_title='Country',
+        yaxis_title='CO2 per capita (metric tons)'
+    )
+    return fig
+   
+
+
+fig = top_n_emitters(df, lower_year, higher_year, nb_displayed)
+
+st.plotly_chart(fig)
+
+df = df.dropna(subset= ['CO2 Per Capita (metric tons)'])
+df = df.sort_values(by="Year")
+
+fig = px.scatter_geo(df,
+                     animation_frame="Year",
+                     locations="Country Code",
+                     color='CO2 Per Capita (metric tons)',
+                     size='CO2 Per Capita (metric tons)',
+                     projection="natural earth")
+st.plotly_chart(fig)
+
+
+
+
+
+
+
